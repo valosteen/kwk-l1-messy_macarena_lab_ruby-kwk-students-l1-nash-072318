ef6d81e0696de94c29394d164f2062e21927b515
valosteen <val_osteen@oneidaschool.org>
2018-07-23 20:43:18 +0000

    automatically backed up by learn

--- macarena.rb
+++ macarena.rb
@@ -9,7 +9,7 @@ puts ""
 puts "Step 1: Right arm out in front of you, palm facing down."
 
 # Step 2: FIXME
-puts "Step 2: left arm out in front of you, palm facing down"
+puts "Step 2: Left arm out in front of you, palm facing down"
 
 # Step 3: FIXME
 puts "Step 3: Place your right hand on your left shoulder."
